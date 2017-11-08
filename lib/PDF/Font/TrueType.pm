46d8d37960b968c5e0a86e3ae409a5a7e458fdb8
David Warring <david.warring@gmail.com>
2017-11-09 08:31:22 +1300

    add basic TrueType embedding
    works for Win encoding subset only, but with a fully embedded font.

--- lib/PDF/Font/TrueType.pm
+++ lib/PDF/Font/TrueType.pm
@@ -6,11 +6,44 @@ class PDF::Font::TrueType {
     use Font::FreeType::Face;
 
     has Font::FreeType::Face $.face;
-    has PDF::Content::Font::Enc::Type1 $!encoder handles <encode decode enc>;
+    has PDF::Content::Font::Enc::Type1 $!encoder handles <decode enc>;
     has Blob $.font-stream is required;
+    use PDF::Content::Font;
+    has PDF::Content::Font $!dict;
+    has Int $!first-char;
+    has Int $!last-char;
+    has int16 @!widths;
 
     submethod TWEAK(:$enc = 'win') {
         $!encoder = PDF::Content::Font::Enc::Type1.new: :$enc;
+        @!widths[255] = 0;
+    }
+
+    method encode(Str $text, :$str) {
+        my buf8 $encoded = $!encoder.encode($text);
+
+        given $.to-dict {
+            my $min = $encoded.min;
+            my $max = $encoded.max;
+
+            my $to-unicode := $!encoder.to-unicode;
+            for $encoded.list {
+                @!widths[$_] ||= $.stringwidth($to-unicode[$_].chr).round;
+            }
+
+            if !$!first-char || $min < $!first-char {
+                .<FirstChar> = $!first-char = $min;
+            }
+
+            if !$!last-char || $max > $!last-char {
+                .<LastChar> = $!last-char = $max;
+            }
+
+            .<Widths> = @!widths[$!first-char .. $!last-char];
+        }
+        $str
+            ?? $encoded.decode('latin-1')
+            !! $encoded;
     }
 
     method !font-descriptor {
@@ -35,19 +68,30 @@ class PDF::Font::TrueType {
     }
 
     method to-dict {
-        my %enc-name = :win<WinAnsiEncoding>, :mac<MacRomanEncoding>;
-        my $FontDescriptor = self!font-descriptor;
-        my $BaseFont = $FontDescriptor<FontName>;
-        my $dict = { :Type( :name<Font> ), :Subtype( :name<TrueType> ),
-                     :$BaseFont,
-                     :$FontDescriptor,
-        };
+        $!dict //= do {
+            my %enc-name = :win<WinAnsiEncoding>, :mac<MacRomanEncoding>;
+            my $FontDescriptor = self!font-descriptor;
+            my $BaseFont = $FontDescriptor<FontName>;
+            my $dict = { :Type( :name<Font> ), :Subtype( :name<TrueType> ),
+                         :$BaseFont,
+                         :$FontDescriptor,
+            };
 
-        with %enc-name{self.enc} -> $name {
-            $dict<Encoding> = :$name;
+            with %enc-name{self.enc} -> $name {
+                $dict<Encoding> = :$name;
+            }
+
+            PDF::Content::Font.make-font(
+                PDF::DAO::Dict.coerce($dict),
+                self);
         }
+    }
 
-        PDF::DAO.coerce: :$dict;
+    multi method stringwidth(Str $str is copy, $pointsize = 1000, Bool :$kern=False) {
+        $str = 'i' if $str eq ' '; # hack
+        $!face.set-char-size($pointsize, $pointsize, 72, 72);
+        my $vec = $!face.measure-text( $str, :$kern);
+        $vec.x;
     }
 
 }
--- t/true-type.t
+++ t/true-type.t
@@ -7,10 +7,20 @@ use Test;
 srand(123456);
 my $pdf = PDF::Lite.new;
 my $page = $pdf.add-page;
+my $font = PDF::Font.load-font("t/fonts/DejaVuSans.ttf");
 
 $page.text: {
-   .font = .use-font: PDF::Font.load-font("t/fonts/DejaVuSans.ttf");
+   .font = .use-font: $font;
    .text-position = [10, 10];
    .say: 'Hello, world';
 }
-$pdf.save-as: "t/true-type.pdf";
+$page = $pdf.add-page;
+$page.text: {
+   .font = .use-font: $font;
+   .text-position = [10, 10];
+   .say: 'Bye, for now';
+}
+lives-ok { $pdf.save-as: "t/true-type.pdf" };
+
+done-testing;
+
