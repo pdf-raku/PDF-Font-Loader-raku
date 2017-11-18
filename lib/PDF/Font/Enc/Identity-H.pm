d06db5ab33773c43e35b65aa9f5390e2fb646bab
David Warring <david.warring@gmail.com>
2017-11-18 15:57:30 +1300

    setup identity-h decoding

--- lib/PDF/Font/Enc/Identity-H.pm
+++ lib/PDF/Font/Enc/Identity-H.pm
@@ -1,10 +1,11 @@
 class PDF::Font::Enc::Identity-H {
 
     use Font::FreeType::Face;
+    use Font::FreeType::Native::Types;
     use PDF::DAO;
 
     has Font::FreeType::Face $.face is required;
-    has uint32 @.to-unicode;
+    has uint32 @!to-unicode;
 
     multi method encode(Str $text, :$str! --> Str) {
         my $hex-string = self.encode($text).decode: 'latin-1';
@@ -20,6 +21,30 @@ class PDF::Font::Enc::Identity-H {
             @codes.push: $index mod 256;
         }
         buf8.new: @codes;
+    }
+
+      method !setup-decoding {
+          my $struct = $!face.struct;
+          my FT_UInt $glyph-idx;
+          my FT_ULong $char-code = $struct.FT_Get_First_Char( $glyph-idx);
+          while $glyph-idx {
+              @!to-unicode[ $glyph-idx ] = $char-code;
+              $char-code = $struct.FT_Get_Next_Char( $char-code, $glyph-idx);
+          }
+    }
+
+    method to-unicode {
+        state $ = self!setup-decoding;
+        @!to-unicode;
+    }
+
+    multi method decode(Str $encoded, :$str! --> Str) {
+        my @to-unicode := self.to-unicode;
+        $encoded.ords.map( -> \hi, \lo {@to-unicode[hi +< 8 + lo]}).grep({$_}).map({.chr}).join;
+    }
+    multi method decode(Str $encoded --> buf32) {
+        my @to-unicode := self.to-unicode;
+        buf32.new: $encoded.ords.map( -> \hi, \lo {@to-unicode[hi +< 8 + lo]}).grep: {$_};
     }
 
 }
